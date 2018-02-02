87b02f5225d49ad9fc5f824763653ceca1fd32c4
Tien Manh Do <domanhtien2011@gmail.com>
2018-02-02 20:33:08 +0700

    Delete unnecessary elements in homepage and adding axios

---
+++ src/containers/LostPets/LostPets.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import React, { Component } from 'react';
+
+class LostPets extends Component {
+  render() {
+
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LostPets);
