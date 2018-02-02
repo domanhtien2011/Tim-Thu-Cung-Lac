f9818189455c381730acf9f0a74d77707c8c428f
Tien Manh Do <domanhtien2011@gmail.com>
2018-02-02 17:08:34 +0700

    Finish settup redux, middleware, and folder structures

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {
+  createStore,
+  applyMiddleware,
+  compose
+} from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './store/reducers';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatching', action);
+      const result = next(action);
+      console.log('[Middleware] next state', store.getState());
+      return result;
+    }
+  }
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(logger, thunk)));
+
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </ Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
---
+++ src/store/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// import PomodoroReducer from './pomodoro_reducer';
+
+const rootReducer = combineReducers({
+  // timer: PomodoroReducer
+})
+
+export default rootReducer
