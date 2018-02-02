fb2827281a77acdba5a53471ec1c7c93b43bbc21
Tien Manh Do <domanhtien2011@gmail.com>
2018-02-02 17:29:57 +0700

    Finish adding semanti ui for react, home page layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import HomepageLayout from './layouts/HomePage'
 
 class App extends Component {
   render() {
     return (
-      <div>Tìm Thú Lạc</div>
+      <HomepageLayout />
     );
   }
 }
